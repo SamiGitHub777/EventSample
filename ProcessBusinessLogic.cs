b4fc6b76f3822927b8e42b6df21b7fd778bf8c3c
Sami Benabidallah <sami.benabidallah@gmail.com>
2021-12-01 14:52:38 +0100

    first commit

---
+++ ProcessBusinessLogic.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EventSample
+{
+    public class ProcessBusinessLogic
+    {
+        public event EventHandler<ProcessEventArgs> ProcessCompleted;
+
+        public void StartProcess()
+        {
+            var data = new ProcessEventArgs();
+            try
+            {
+                Console.WriteLine("Process started");
+                Thread.Sleep(3000);
+                data.IsSuccessful = true;
+                data.CompletionDateTime = DateTime.Now;
+                OnProcessCompleted(data);
+            }
+            catch
+            {
+                Console.WriteLine("Error");
+                data.IsSuccessful = false;
+                data.CompletionDateTime = DateTime.Now;
+                OnProcessCompleted(data);
+            }
+        }
+
+        private void OnProcessCompleted(ProcessEventArgs data)
+        {
+            ProcessCompleted?.Invoke(this, data);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace EventSample
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            ProcessBusinessLogic processBusinessLogic = new ProcessBusinessLogic();
+            processBusinessLogic.ProcessCompleted += OnCompleted;
+            processBusinessLogic.StartProcess();
+            Console.ReadLine();
+        }
+
+        private static void OnCompleted(object sender, ProcessEventArgs eventArgs)
+        {
+            Console.WriteLine($"Main : OnCompleted. Is Successfull : {eventArgs.IsSuccessful}, Completion time : {eventArgs.CompletionDateTime}");
+        }
+    }
+}
